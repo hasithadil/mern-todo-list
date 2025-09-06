1398c0f8a200d1e29f007bc5a0892fc796231a99
hasithadil <kah.dilshan@gmail.com>
2025-09-06 09:46:47 +0530

    create frontend

---
+++ TODO-List/src/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Create from './Create'
+
+function Home() {
+
+    const [todos, setTodos] = useState([])
+
+  return (
+    <div>
+        <h2>TODO LIST</h2>
+        <Create />
+
+        {
+            todos.length === 0 ?
+                <div>
+                    <h2>No Records</h2>
+                </div>
+                : 
+                todos.map(todo => {
+                    <div>
+                        {todo}
+                    </div>
+                })
+        }
+    </div>
+  )
+}
+
+export default Home
